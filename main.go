62f07553dfee379c2ff84dce0a279da0ba0c36c2
Vic van Gool <vic@cloud66.com>
2016-06-27 16:01:29 +0100

    env var

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world again!\n")
+	io.WriteString(w, "Fetching data from another service!\n")
 	var sourceURL = os.Getenv("DATA_SOURCE")
 	if sourceURL == "" {
 		sourceURL = "http://web.cloud66.local:3000/counts"
@@ -17,17 +17,17 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get(sourceURL)
 	if err != nil {
 		// handle error
-		io.WriteString(w, fmt.Sprintf("Count from %s: <<unavailable>>", sourceURL))
+		io.WriteString(w, fmt.Sprintf("Count from %s is \"<<unavailable>>\"", sourceURL))
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// handle error
-		io.WriteString(w, fmt.Sprintf("Count from %s: <<unavailable>>", sourceURL))
+		io.WriteString(w, fmt.Sprintf("Count from %s is \"<<unavailable>>\"", sourceURL))
 		return
 	}
-	io.WriteString(w, fmt.Sprintf("Count from %s: %s", sourceURL, string(body)))
+	io.WriteString(w, fmt.Sprintf("Count from %s is \"%s\"", sourceURL, string(body)))
 }
 
 func main() {
