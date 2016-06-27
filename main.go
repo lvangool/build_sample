b90624b4a8b4b39414a131d35ffad055edfd0784
Vic van Gool <vic@cloud66.com>
2016-06-27 15:32:05 +0100

    env var

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world again!\n")
-	resp, err := http.Get("http://web.cloud66.local:3001/counts")
+	var sourceDNS = os.Getenv("COUNT_SOURCE")
+	if sourceDNS == "" {
+		sourceDNS = "web.cloud66.local"
+	}
+	var sourceURL = fmt.Sprintf("http://%s:3001/counts", sourceDNS)
+	resp, err := http.Get(sourceURL)
 	if err != nil {
 		// handle error
-		io.WriteString(w, "Count from web.cloud66.local is: <<unavailable>>")
+		io.WriteString(w, fmt.Sprintf("Count from %s: <<unavailable>>", sourceURL))
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// handle error
-		io.WriteString(w, "Count from web.cloud66.local is: <<unavailable>>")
+		io.WriteString(w, fmt.Sprintf("Count from %s: <<unavailable>>", sourceURL))
 		return
 	}
-	io.WriteString(w, "Count from web.cloud66.local is: "+string(body))
+	io.WriteString(w, fmt.Sprintf("Count from %s: %s", sourceURL, string(body)))
 }
 
 func main() {
